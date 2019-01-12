729625aecfa1b7986d2ff81a6adf1c85cef2fa87
Dylan Dinh <dylan.dinh12@gmail.com>
2019-01-12 16:27:06 +0100

    clean modularity

--- we-are-open/Screens/Welcome.js
+++ we-are-open/Screens/Welcome.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text , StyleSheet} from 'react-native'
+import { View, Text , StyleSheet, ScrollView} from 'react-native'
 import firebase from 'firebase'
 import * as ReactNavigation from 'react-native-elements';
 import AddNewApi from '../AddNewApi'
@@ -22,33 +22,28 @@ export default class Welcome extends React.Component {
 
     render() {   
         return (
-            <View style = {styles.container}>
+            <ScrollView style = {styles.container}>
 
-<ReactNavigation.Card
-  title='THIS IS THE GOOGLE API'
-  image={require('../assets/risitaspeur.png')}>
-  <Text style={{marginBottom: 10}}>
-    Here you can post new event through our app or changing info
-  </Text>
-  <ReactNavigation.Button
-    backgroundColor='#03A9F4'
-    onPress= { () => this.props.navigation.navigate('GoogleBusinessApi')}
-    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-    title='VIEW GOOGLE API NOW' />
-</ReactNavigation.Card>
+                <AddNewApi
+                    buttonStyles = {styles.input}
+                    cardTitle = {'Google Business API'}
+                    image = {require('../assets/risitaspeur.png')}
+                    content = {'Here you can post new event through our app or changing info'}
+                    buttonTitle = {'View Google Business API'}
+                    onPress = { () => this.props.navigation.navigate('GoogleBusinessApi')}
+                />
 
+                <AddNewApi
+                    buttonStyles = {styles.input}
+                    cardTitle = {'New card'}
+                    image = {require('../assets/risitaspeur.png')}
+                    content = {'Custom text'}
+                    buttonTitle = {'View new API'}
+                    onPress = {() => this.props.navigation.navigate('GoogleBusinessApi')}
+                />
 
-<AddNewApi
-        buttonStyles = {styles.input}
-        content = {'Custom text'}
-        image = {require('../assets/risitaspeur.png')}
-        cardTitle = {'New card'}
-        buttonTitle = {'View new api'}
-        onPress = {() => this.props.navigation.navigate('GoogleBusinessApi')}
-    />
-
-<ReactNavigation.Button title="Disconnect"
-    buttonStyle={{
+                <ReactNavigation.Button title="Disconnect"
+                    buttonStyle={{
                     backgroundColor: "#2f2d30",
                     padding: 1,
                     margin: 15,
@@ -56,13 +51,12 @@ export default class Welcome extends React.Component {
                     borderWidth: 1,
                     borderRadius: 5
                     }}
-
-    onPress = {  
-        () => firebase.auth().signOut().then(function() {
-    }).catch(function(error) {
-        }) } 
-    />   
-            </View>
+                    onPress = {  
+                        () => firebase.auth().signOut().then(function() {
+                        }).catch(function(error) {
+                        }) } 
+                />   
+            </ScrollView>
         )
     }
 }
@@ -72,11 +66,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#2f2d30',
         flex: 1
     },
-    color: {
-        backgroundColor: '#2f2d30'
-    },
-
-
+    
     input: {
         backgroundColor: "#2f2d30",
         padding: 1,
@@ -91,14 +81,18 @@ const styles = StyleSheet.create({
        height: 40,
        borderColor: '#7a42f4',
        borderWidth: 1
-    },
-
-    submitButton: {
-       padding: 1,
-       margin: 15,
-       height: 40
-    },
-    submitButtonText:{
-       color: 'white'
     }
  })
+
+ /*<ReactNavigation.Card
+  title='THIS IS THE GOOGLE API'
+  image={require('../assets/risitaspeur.png')}>
+  <Text style={{marginBottom: 10}}>
+    Here you can post new event through our app or changing info
+  </Text>
+  <ReactNavigation.Button
+    backgroundColor='#03A9F4'
+    onPress= { () => this.props.navigation.navigate('GoogleBusinessApi')}
+    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+    title='VIEW GOOGLE API NOW' />
+</ReactNavigation.Card> */
